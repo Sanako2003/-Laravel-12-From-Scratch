fc0f5eacac8323304cb0a3ac08727f27e2cbf2ce
SanaKorj <sanakorj43@gmail.com>
2025-05-23 21:00:37 +0300

    Starter Kits and Using Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-// use Illuminate\Support\Facades\DB;
 
-use App\Http\Controllers\PostController;
- 
-Route::get('posts/{post}', [PostController::class, 'show']) 
-    ->name('post.show'); 
- 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/second', 'second'); 
 
-Route::get('/third', function () {
-    return view('third'); // <-- third.blade.php file doesn't exist
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/', [HomeController::class, 'index'])
-    ->name('home');
-Route::view('contact', 'contact')->name('contact');
-Route::view('about', 'about')->name('about');
-Route::view('article', 'article')->name('article');
-
-Route::get('posts/{postId}', [SomeController::class, 'some_method']);
+require __DIR__.'/auth.php';
